ae04772b3b45f53a928fde13b83014fbcbcb61a7
Phyberosis <Phyberosis@gmail.com>
2020-03-06 12:14:27 -0800

    full page

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-;(function () {
-    console.log('IIFE JS Loaded!')
-  })()
+let canvas, ctx
+
+let test;
+
+function init () {
+  // set our config variables
+  canvas = document.getElementById('myCanvas')
+  ctx = canvas.getContext('2d')
+
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  ctx.beginPath()
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+}
+document.addEventListener('DOMContentLoaded', init)
+
+let pendingTask;
+function beginTicks(){
+  pendingTask = new Promise(tick, resolve, reject);
+}
+
+function tick(resolve, reject){
+  scheduleNext(resolve);
+}  
+
+function scheduleNext(){
+  pendingTask = new Promise(tick, resolve, reject);
+}
